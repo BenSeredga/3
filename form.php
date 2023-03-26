4847117a7f8f0f6d3a03fc8505546cf0bed864c9
BenSeredga <112845645+BenSeredga@users.noreply.github.com>
2023-03-27 00:18:53 +0300

    Add files via upload

--- form.php
+++ form.php
@@ -1,12 +1,120 @@
-<form action="" method="POST">
-  <input name="fio" />
-  <select name="year">
-    <?php 
-    for ($i = 1922; $i <= 2022; $i++) {
-      printf('<option value="%d">%d год</option>', $i, $i);
-    }
-    ?>
-  </select>
-  
-  <input type="submit" value="ok" />
-</form>
+<!DOCTYPE html>
+<html lang="ru" style="min-width:400px; overflow-x:auto;">
+<head>
+  <title>Forms</title>
+  <!--link rel="stylesheet" href="style.css" type="text/css"-->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+</head>
+<body class ="container d-flex flex-column">
+  <form action="" method="POST" class="ms-auto me-auto d-flex flex-column justify-content-center">
+        <p class="text-center fw-bold fs-2">Форма</p>
+        <br>
+
+        <div class="mb-3">
+
+          <label for="nameinput" class="form-label">Ваше Имя</label>
+          <input name="name" class="form-control" id="nameinput" placeholder="Иванов Иван Иванович">
+
+          <label for="emailinput" class="form-label">Электронная почта</label>
+          <input name="email" class="form-control" id="emailinput" placeholder="name@example.com">
+
+          <label for="date1" class="form-label">Выберите Год</label>
+          <select name="year" class="form-control" id="date1">
+          <?php
+          for ($i = 1923; $i <= 2023; $i++) {
+          printf('<option value="%d">%d год</option>', $i, $i);
+          }
+          ?>
+          </select>
+        </div>
+        
+        <div class="container-fluid mb-4 ">
+          <label for="gender" class="form-label">Пол</label>
+          <select class="form-control" name="gender" id="gender" size="2">
+            <option value="male">Мужской</option>
+            <option value="female">Женский</option>
+          </select>
+
+        </div>
+
+        
+
+        <!--div class="container-fluid btn-group mb-4 " role="group">
+          
+
+          <input type="radio" class="btn-check" name="gender" id="sex1">
+          <label class="btn btn-outline-primary" for="sex1">Мужской</label>
+
+          <input type="radio" class="btn-check" name="gender" id="sex2" >
+          <label class="btn btn-outline-primary" for="sex2">Женский</label>
+        </div-->
+
+        <!--p class="text-center">Количество конечностей</p-->
+
+        <div class="container-fluid mb-4 ">
+          <label for="limbs" class="form-label">Количество конечностей</label>
+          <select class="form-control" name="limbs" id="limbs" size="4">
+            <option value="1">Одна</option>
+            <option value="2">Две</option>
+            <option value="3">Три</option>
+            <option value="4">Четыре</option>
+          </select>
+
+        </div>
+
+        <!--div class="container-fluid btn-group mb-3" role="group">
+
+          <input type="radio" class="btn-check" name="options" id="1">
+          <label class="btn btn-outline-primary" for="option1">Одна</label>
+
+          <input type="radio" class="btn-check" name="options" id="2" >
+          <label class="btn btn-outline-primary" for="option2">Две</label>
+
+          <input type="radio" class="btn-check" name="options" id="3" >
+          <label class="btn btn-outline-primary" for="option3">Три</label>
+
+          <input type="radio" class="btn-check" name="options" id="4" >
+          <label class="btn btn-outline-primary" for="option4">Четыре</label>
+
+        </div-->
+
+        <div class="text-center">
+          <label class="" for="superpowers" id="superpowers_label">Суперспособности</label><br>
+          <select class="form-select mb-3" name="abilities[]" multiple = "multiple" id="superpowers">
+            
+            <option value="1">Бессмертие</option>
+            <option value="2">Прохождение сквозь стены</option>
+            <option value="3">Левитация</option>
+            <option value="4">флэш бег</option>
+          </select>
+        </div>
+
+        <div class="text-center mb-4">
+          <label class="form-label" for="biography">Биография</label><br>
+          <textarea name="biography" class="form-control" id="biography" aria-label="With textarea" placeholder="Расскажите о себе"></textarea>
+        </div>
+
+        <div class="text-center mb-5">
+          <input name="checkbox" type="checkbox" class="btn-check" id="confirm">
+          <label class="btn btn-outline-primary" for="confirm">Чекбокс</label>
+        </div>
+        <div class="text-center mb-4">
+          <input class="btn btn-success col-6" type="submit" value="Отправить">
+        </div>
+      </form>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+  <!--script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script-->
+
+
+  <!--script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7 /js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script-->
+
+  <script src="script.js" defer></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,67 +2,54 @@
 // Отправляем браузеру правильную кодировку,
 // файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
-print_r($_POST);
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-// В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
-if (!empty($_GET['save'])) {
-// Если есть параметр save, то выводим сообщение пользователю.
-print('Спасибо, результаты сохранены.');
+  include('form.php');
+  if (!empty($_GET['save'])) {
+    print('результаты сохранены');
+  }
+  exit();
 }
-// Включаем содержимое файла form.php.
-include('form.php');
-// Завершаем работу скрипта.
-exit();
-}
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
 
 // Проверяем ошибки.
 $errors = FALSE;
-if (empty($_POST['fio'])) {
-print('Заполните имя.<br/>');
-$errors = TRUE;
+
+if (empty($_POST['name'])) {
+  print('Укажите имя.<br/>');
+  $errors = TRUE;
+}
+
+if (empty($_POST['year']) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
+  print('Укажите год.<br/>');
+  $errors = TRUE;
 }
 
 if (empty($_POST['email'])) {
-print('Заполните email.<br/>');
-$errors = TRUE;
+  print('Укажите email.<br/>');
+  $errors = TRUE;
 }
 
-if (empty($_POST['checkbox']) || !($_POST['checkbox'] == 'on' || $_POST['checkbox'] == 1)) {
-print('Подтвердите.<br/>');
-$errors = TRUE;
+if($_POST['gender'] !== 'male' && $_POST['gender'] !== 'female'){
+  print('Неверный пол<br/>');
+  $errors = TRUE;
 }
 
-if (empty($_POST['abilities'])) {
-print('Выберите способности.<br/>');
-$errors = TRUE;
+if($_POST['limbs'] !== '1' && $_POST['limbs'] !== '2' && $_POST['limbs'] !== '3' && $_POST['limbs'] !== '4'){  
+  print('Укажите количество конечностей<br/>');
+  $errors = TRUE;
 }
 
-if (empty($_POST['limbs']) || !is_numeric($_POST['limbs']) || !preg_match('/^\d+$/', $_POST['limbs'])) {
-print('Все так плохо?<br/>');
-$errors = TRUE;
+if (empty($_POST['abilities'])) {  
+    print('Укажите способности<br/>');
+    $errors = TRUE;
 }
 
-if (empty($_POST['gender'])) {
-print('Вы кто?<br/>');
-$errors = TRUE;
+if (empty($_POST['checkbox']) || !($_POST['checkbox'] == 'on' || !$_POST['checkbox'] == 1)) {
+    print('Чекбокс<br/>');
+    $errors = TRUE;
 }
 
-if (empty($_POST['year'])) {
-print('Заполните год.<br/>');
-$errors = TRUE;
-}
+if ($errors) {exit();}
 
-// *************
-// Тут необходимо проверить правильность заполнения всех остальных полей.
-// *************
-
-if ($errors) {
-// При наличии ошибок завершаем работу скрипта.
-exit();
-}
 // Сохранение в базу данных.
 
 $user = 'u52807';
@@ -71,42 +58,31 @@ $db = new PDO('mysql:host=localhost;dbname=u52807', $user, $pass, [PDO::ATTR_PER
 
 // Подготовленный запрос. Не именованные метки.
 try {
-$stmt = $db->prepare("INSERT INTO users SET name = ?, year = ?, biography = ?, email = ?, limbs = ?, gender = ?, checkbox = ?");
-$stmt->execute([$_POST['fio'], $_POST['year'], $_POST['biography'], $_POST['email'], $_POST['limbs'], $_POST['gender'], 1]);
-} catch (PDOException $e) {
-print('Error : ' . $e->getMessage());
-exit();
+  $stmt = $db->prepare("INSERT INTO application SET name = ?, year = ?, email = ?,  gender = ?, checkbox = ?, biography = ?, limbs = ?");
+  $stmt->execute([$_POST['name'], $_POST['year'], $_POST['email'], $_POST['gender'], 1, $_POST['biography'], $_POST['limbs']]);
+  if(!$stmt){print('Error: ' . $stmt->errorInfo());}
+} 
+catch (PDOException $e) {
+  print('Error : ' . $e->getMessage());
+  exit();
 }
 
-$user_id = $db->lastInsertId();
+$person_id = $db->lastInsertId();
 
-foreach ($_POST['abilities'] as $ability) {
-try {
-$stmt = $db->prepare("INSERT INTO abilities SET user_id = ?, name = ?");
-$stmt->execute([$user_id, $ability]);
-} catch (PDOException $e) {
-print('Error : ' . $e->getMessage());
-exit();
+foreach ($_POST['abilities'] as $sup_id) {
+  try {
+    $stmt = $db->prepare("INSERT INTO connects SET person_id = ?, sup_id = ?");
+    $stmt->execute([$person_id, $sup_id]);
+    if (!$stmt) {print('Error : ' . $stmt->errorInfo());}
+  } 
+  catch (PDOException $e) {
+    print('Error : ' . $e->getMessage());
+    exit();
+  }
 }
-}
-// stmt - это "дескриптор состояния".
 
-// Именованные метки.
-//$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
-//$stmt -> execute(['label'=>'perfect', 'color'=>'green']);
+header('Location: ?save=1');
+?>
 
-//Еще вариант
-/*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
-$stmt->bindParam(':firstname', $firstname);
-$stmt->bindParam(':lastname', $lastname);
-$stmt->bindParam(':email', $email);
-$firstname = "John";
-$lastname = "Smith";
-$email = "john@test.com";
-$stmt->execute();
-*/
 
-// Делаем перенаправление.
-// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
-// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
-// header('Location: ?save=1');
+
